c293d39a2a44cff9540aa1195181a28d5343b296
behnam-ghafouri <behnam.ghafouri@gmail.com>
2020-06-29 15:51:00 -0400

    first commit

---
+++ src/yourdata.js
@@ -0,0 +1,85 @@
+export default
+    {
+        //(Please Do Not Remove The comma(,) after every variable)
+        //Change The Website Template
+        name: 'Behnam Ghafouri',
+        headerTagline: [//Line 1 For Header
+            'Hi, i am ',
+            //Line 2 For Header
+            'Full Stack Developer',
+            //Line 3 For Header
+            ''
+        ],
+        //Contact Email
+        contactEmail: 'beghafou@gmail.com',
+        // Add Your About Text Here
+        abouttext: "Resourceful and collaborative web developer currently working at Ministry of Government & Consumer Services. Highly adaptable professional who embraces teamwork and enjoys working independently. Always passionate about learning and problem solving.",
+        aboutImage: '',
+        //Change This To Hide The Image of About Section (True Or False)
+        ShowAboutImage: true,//true or false (Change Here)
+        // Change Projects Here 
+        projects: [
+            {
+                id: 1,//DO NOT CHANGE THIS (Please)😅
+                title: 'Project One', //Project Title - Add Your Project Title Here
+                service: 'The GoCloudClients web application (GoCloudClients)', // Add Your Service Type Here
+                //Project Image - Add Your Project Image Here
+                imageSrc: "https://res.cloudinary.com/dpk96y6hx/image/upload/v1593452786/GoCloudClients_dgjt3m.jpg",
+                //Project URL - Add Your Project Url Here
+                url: 'http://chetanverma.com/'
+            },
+            {
+                id: 2,//DO NOT CHANGE THIS (Please)😅
+                title: 'Project Two',
+                service: 'Website Development',
+                imageSrc: "https://res.cloudinary.com/dpk96y6hx/image/upload/v1593456328/GoCloudWebsite_qm7o1l.jpg",
+                url: 'http://chetanverma.com/'
+            },
+            // {
+            //     id: 3,//DO NOT CHANGE THIS (Please)😅
+            //     title: 'Project Three',
+            //     service: 'Web App',
+            //     imageSrc: "https://res.cloudinary.com/dpk96y6hx/image/upload/v1593456328/GoCloudWebsite_qm7o1l.jpg",
+            //     url: 'http://chetanverma.com/'
+            // },
+            // {
+            //     id: 4,//DO NOT CHANGE THIS (Please)😅
+            //     title: 'Project Four',
+            //     service: 'Branding',
+            //     imageSrc: "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+            //     url: 'http://chetanverma.com/'
+            // }
+
+            /*
+
+            If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+        ,{
+        id: 5,
+        title: 'Project Five',
+        service: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+        */
+        ],
+        social: [
+            // Add Or Remove The Link Accordingly
+            {
+                name: 'Github',
+                url: 'https://github.com/chetanverma16'
+            },
+            {
+                name: 'Behance',
+                url: 'https://www.behance.net/chetanverma'
+            },
+            {
+                name: 'Dribbble',
+                url: 'https://dribbble.com/chetanverma'
+            },
+            {
+                name: 'Instagram',
+                url: 'https://www.instagram.com/cv.uidesign/'
+            }
+
+        ]
+    }
